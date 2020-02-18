84701e0fc5640eae2fab5ee347a4d595e4f790a1
jamilchan <jamilchan@Sonics-iMac.local>
2020-02-18 10:52:24 +0700

    converting to repository pattern

---
+++ app/Repositories/JenisBahan/JenisBahanRepositoryInterface.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repositories\JenisBahan\JenisBahanRepositoryInterface;
+
+interface JenisBahanRepositoryInterface {
+
+    /**
+     * method untuk mendapatkan semua jenis_bahan
+     * @return object
+     */
+    public function all() : object;
+
+    /**
+     * method untuk mendapatkan jenis_bahan berdasarkan kode
+     * @param string
+     * @return object
+     */
+    public function get(string $kode) : object;
+
+    /**
+     * method untuk membuat jenis_bahan
+     * @param object
+     * @return object
+     */
+    public function create(object $jenis_bahan) : object;
+
+    /**
+     * method untuk memperbaharui jenis_bahan
+     * @param string
+     * @param object
+     * @return object
+     */
+    public function update(string $kode, object $jenis_bahan) : object;
+
+    /**
+     * method untuk menghapus jenis_bahan
+     * @param int
+     * @return object
+     */
+    public function delete(string $kode) : object;
+}
